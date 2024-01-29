5e62964f1a66a4db7a25433e3e78d0ae7b7eaef1
RAKOTONDRABE_Ph <p.rakotondrabe@lprs.fr>
2024-01-29 12:28:29 +0100

    #1 Création template CRUD user

---
+++ src/main/java/controller/Controller/FournisseurCrudController.java
@@ -0,0 +1,8 @@
+package controller.Controller;
+
+public class FournisseurCrudController {
+
+
+
+
+}
---
+++ src/main/java/graphicController/FournisseurCrud.java
@@ -0,0 +1,68 @@
+package graphicController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class FournisseurCrud {
+
+    @FXML
+    private TableColumn<?, ?> department;
+
+    @FXML
+    private TableColumn<?, ?> eid;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Label lavel;
+
+    @FXML
+    private TableColumn<?, ?> mobile;
+
+    @FXML
+    private TableColumn<?, ?> name;
+
+    @FXML
+    private TextField nom;
+
+    @FXML
+    private TextField prenom;
+
+    @FXML
+    private TextField role;
+
+    @FXML
+    private TableColumn<?, ?> salary1;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    void deleteUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void register(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showAll(ActionEvent event) {
+
+    }
+
+    @FXML
+    void update(ActionEvent event) {
+
+    }
+
+}
