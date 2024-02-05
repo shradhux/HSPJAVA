eaa33b66038de861bbab3ad2e17336622ec80da1
KALLEE-DIGUMBER_Sh <s.kallee-digumber@lprs.fr>
2024-02-05 11:02:12 +0100

    In progress - ajout du nom dans la classe Fournisseur

--- src/main/java/Classes/Fournisseur.java
+++ src/main/java/Classes/Fournisseur.java
@@ -3,6 +3,8 @@ package Classes;
 public class Fournisseur {
     private static int id;
 
+    private String nom;
+
     public static int getId() {
         return id;
     }
@@ -10,4 +12,8 @@ public class Fournisseur {
     public static void setId(int id) {
         Fournisseur.id = id;
     }
+    public  String getNom() {
+        return this.nom;
+    }
+    public void setNom(String nom) {this.nom =nom;}
 }
