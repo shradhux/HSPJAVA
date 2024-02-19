4a55f615da00944510d52dd1592e8f5bcc4ead26
RAKOTONDRABE_Ph <p.rakotondrabe@lprs.fr>
2024-02-19 12:12:24 +0100

    #4 in progress - Crud user partie admin

--- src/main/java/graphicController/UserCrud.java
+++ src/main/java/graphicController/UserCrud.java
@@ -1,11 +1,14 @@
 package graphicController;
-
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Random;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import modele.bdd.Bdd;
 
 public class UserCrud {
 
@@ -22,7 +25,7 @@ public class UserCrud {
     private TextField id;
 
     @FXML
-    private Label lavel;
+    private Label label;
 
     @FXML
     private TableColumn<?, ?> mobile;
@@ -46,17 +49,41 @@ public class UserCrud {
     private TableView<?> table;
 
     @FXML
+    public void initialize() {
+
+
+
+
+    }
+
+
+
+
+    @FXML
     void deleteUser(ActionEvent event) {
 
     }
 
     @FXML
     void register(ActionEvent event) {
-
+        PreparedStatement req = null;
+        try {
+            req = new Bdd().getBdd().prepareStatement("INSERT INTO utilisateur (nom, prenom, email, role) VALUES (?,?,?,?) ");
+            req.setString(1, this.nom.getText());
+            req.setString(2, this.nom.getText());
+            req.setString(3, this.nom.getText());
+            req.setString(4, this.nom.getText());
+            req.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void showAll(ActionEvent event) {
+
+
+
 
     }
 
